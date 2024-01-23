e6f4b2b75b6e9d9a218fbe8a1bc2e77e85ba242f
CodeSandbox <support@codesandbox.io>
2024-01-23 12:22:57 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello CodeSandbox");
+const redux = require("redux");
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+const createStore = redux.createStore;
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("Initial State ", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State ", store.getState());
+});
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
